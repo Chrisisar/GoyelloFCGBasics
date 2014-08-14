f7ded943224099abd37b7222f605c7395ca19db8
Chrisisar <krzysztofgornowicz@gmail.com>
2014-08-14 15:19:04 +0200

    upload
    lel

---
+++ AddressBook/Controllers/AddressBookController.cs
@@ -0,0 +1,72 @@
+﻿using AddressBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AddressBook.Controllers
+{
+    public class AddressBookController : Controller
+    {
+        //
+        // GET: /AddressBook/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("ShowContactList");
+        }
+
+        public ViewResult AddNewContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewContact(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                ContactList.AddressBook.Add(contact);
+                return RedirectToAction("ShowContactList");
+            }
+            return View();
+            
+        }
+
+        public ActionResult EditContact(int ContactID)
+        {
+            return View(ContactList.AddressBook.Last(m => m.ContactID == ContactID));
+        }
+
+        [HttpPost]
+        public ActionResult EditContact(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                ContactList.AddressBook[ContactList.AddressBook.FindIndex(m => m.ContactID == contact.ContactID)] = contact;
+                return RedirectToAction("ShowContactList");
+            }
+            return View(contact);
+        }
+
+        public ActionResult RemoveContact(int ContactID)
+        {
+            ContactList.AddressBook.Remove(ContactList.AddressBook.Find(m => m.ContactID == ContactID));
+            return RedirectToAction("ShowContactList");
+        }
+
+        public ActionResult ChangeFavorite(int ContactID)
+        {
+            ContactList.AddressBook[ContactList.AddressBook.FindIndex(m => m.ContactID == ContactID)].IsFavorite = !ContactList.AddressBook[ContactList.AddressBook.FindIndex(m => m.ContactID == ContactID)].IsFavorite;
+            return RedirectToAction("ShowContactList");
+        }
+
+        public ViewResult ShowContactList()
+        {
+            
+            return View(ContactList.AddressBook);
+        }
+
+    }
+}
---
+++ AddressBook/Models/Contact.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+        public enum Group {
+            Default = 0,
+            Family = 1,
+            Colleagues = 2,
+            Friends = 3,
+        }
+        public int ContactID { get; set; }
+
+        [Required(ErrorMessage="Name is required")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Address")]
+        public string AddressLine { get; set; }
+        [DisplayName("City")]
+        public string City { get; set; }
+        [DisplayName("Postcode")]
+        public string PostCode { get; set; }
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+        [DisplayName("Email")]
+        public string Email { get; set; }
+        [DisplayName("Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+        [DisplayName("Favorite")]
+        public bool IsFavorite { get; set; }
+        [Required()]
+        [DisplayName("Group")]
+        public Group AssignedTo { get; set; }
+
+        public Contact()
+        {
+            if (ContactList.AddressBook.Any())
+            {
+                ContactID = ContactList.AddressBook.LastOrDefault().ContactID + 1;
+            }
+            else
+            {
+                ContactID = 0;
+            }
+            AssignedTo = Group.Default;
+            IsFavorite = false;
+        }
+    }
+}
