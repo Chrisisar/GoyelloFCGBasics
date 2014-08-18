0d5df81aa61dd5862322174bc3438f4a1a413ea6
Rudauke <jakubj93@gmail.com>
2014-08-18 15:03:49 +0200

    Added GridViewModel.cs

--- AddressBook/Scripts/AddressBook/AddOrEditContact.js
+++ AddressBook/Scripts/AddressBook/AddOrEditContact.js
@@ -2,10 +2,10 @@
 jQuery(document).ready(function () {
     //jQuery('.date').datepicker({ dateFormat: "dd/mm/yy"});
 
-    $.datepicker.setDefaults({ maxDate: new Date() });
-    console.log($(".date").datepicker("option", "minDate"));
-    jQuery(".date").datepicker("option", "maxDate", "+1d");
-    console.log($(".date").datepicker("option", "minDate"));
+    //$.datepicker.setDefaults({ maxDate: new Date() });
+    //console.log($(".date").datepicker("option", "minDate"));
+    //jQuery(".date").datepicker("option", "maxDate", "+1d");
+    //console.log($(".date").datepicker("option", "minDate"));
     jQuery(".date").datepicker({
         maxDate: new Date()
     });
